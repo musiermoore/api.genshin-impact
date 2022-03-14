3b34e9afe9379f8dce01ceae50b26d5e4dd6463f
musiermoore <musiermoore@gmail.com>
2022-03-14 22:41:36 +0600

    Created AuthController with register, login and logout methods

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Requests\AuthRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(AuthLoginRequest $request)
+    {
+        $data = [
+            'login'    => $request->login,
+            'password' => $request->password
+        ];
+
+        if (auth()->attempt($data)) {
+            $user = auth()->user();
+            $token =  $user->createToken('auth-token')->plainTextToken;
+
+            return $this->successResponse(200, 'User logged-in!', ['token' => $token]);
+        }
+
+        return $this->errorResponse(401, 'Unauthorised');
+    }
+
+    public function register(AuthRegisterRequest $request)
+    {
+        $data = [
+            'login'    => $request->login,
+            'password' => Hash::make($request->password)
+        ];
+
+        $user = User::query()->create($data);
+
+        return $this->successResponse(200, 'User successfully registered!', $user);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return $this->successResponse(200, 'The token has been deleted');
+    }
+}
