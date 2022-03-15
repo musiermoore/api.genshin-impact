6650301c23a86e62f4d712535a40ad58e434fcb9
musiermoore <musiermoore@gmail.com>
2022-03-15 20:13:18 +0600

    Added the user relation and methods to get the account data

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,8 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'server_id',
+        'nickname',
+        'uid',
+        'level',
+        'world_level',
+        'gender'
+    ];
+
+    /**
+     * Relations
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function getAccountsByUser($user)
+    {
+        return Account::with('user')->where('user_id', $user->id)->get();
+    }
+
+    public static function getAccountById($accountId)
+    {
+        return Account::with('user')->where('id', $accountId)->first();
+    }
 }
