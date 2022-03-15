a7d15f57c33564dbd417405d695612d3ee378bb4
musiermoore <musiermoore@gmail.com>
2022-03-15 20:15:25 +0600

    Created hasMany relation with accounts

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Relations
+     */
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
 }
