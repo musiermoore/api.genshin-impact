e888f38c43e7c4248b290ce0a730b2775e6340a3
musiermoore <musiermoore@gmail.com>
2022-03-20 18:51:51 +0600

    Created migrations for adding slug to the elements and servers tables

---
+++ database/migrations/2022_03_15_144004_add_slug_column_to_elements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSlugColumnToElementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('elements', function (Blueprint $table) {
+            $table->string('slug')->unique()->after('element');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('elements', function (Blueprint $table) {
+            //
+        });
+    }
+}
