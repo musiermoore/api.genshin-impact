5defdebefc87350c3e9390e22a436a1cac46f9ff
musiermoore <musiermoore@gmail.com>
2022-03-23 20:59:54 +0600

    Added fillable and relations

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Character extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'star_id',
+        'name',
+        'slug',
+        'element_id',
+        'weapon_type_id',
+        'image_path'
+    ];
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
