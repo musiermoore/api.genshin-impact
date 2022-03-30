5ae7b31b63822b02babbb2f8d96340ce63dc0ff7
musiermoore <musiermoore@gmail.com>
2022-03-30 21:30:46 +0600

    Created level service

---
+++ app/Services/LevelService.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Ascension;
+use App\Models\Level;
+
+class LevelService
+{
+    public function getCharacterLevelsWithAscensions()
+    {
+        $characterLevels = Level::getCharacterLevels();
+        $ascensions = Ascension::getCharacterAscensions();
+
+        $levelsWithAscensions = [
+            [
+                'level_id' => $characterLevels[1]['id'],
+                'ascension_id' => $ascensions[0]['id']
+            ],
+            [
+                'level_id' => $characterLevels[20]['id'],
+                'ascension_id' => $ascensions[0]['id']
+            ],
+            [
+                'level_id' => $characterLevels[20]['id'],
+                'ascension_id' => $ascensions[1]['id']
+            ],
+            [
+                'level_id' => $characterLevels[40]['id'],
+                'ascension_id' => $ascensions[1]['id']
+            ],
+            [
+                'level_id' => $characterLevels[40]['id'],
+                'ascension_id' => $ascensions[2]['id']
+            ],
+            [
+                'level_id' => $characterLevels[50]['id'],
+                'ascension_id' => $ascensions[2]['id']
+            ],
+            [
+                'level_id' => $characterLevels[50]['id'],
+                'ascension_id' => $ascensions[3]['id']
+            ],
+            [
+                'level_id' => $characterLevels[60]['id'],
+                'ascension_id' => $ascensions[3]['id']
+            ],
+            [
+                'level_id' => $characterLevels[50]['id'],
+                'ascension_id' => $ascensions[3]['id']
+            ],
+            [
+                'level_id' => $characterLevels[60]['id'],
+                'ascension_id' => $ascensions[3]['id']
+            ],
+            [
+                'level_id' => $characterLevels[60]['id'],
+                'ascension_id' => $ascensions[4]['id']
+            ],
+            [
+                'level_id' => $characterLevels[70]['id'],
+                'ascension_id' => $ascensions[4]['id']
+            ],
+            [
+                'level_id' => $characterLevels[70]['id'],
+                'ascension_id' => $ascensions[5]['id']
+            ],
+            [
+                'level_id' => $characterLevels[80]['id'],
+                'ascension_id' => $ascensions[5]['id']
+            ],
+            [
+                'level_id' => $characterLevels[80]['id'],
+                'ascension_id' => $ascensions[6]['id']
+            ],
+            [
+                'level_id' => $characterLevels[90]['id'],
+                'ascension_id' => $ascensions[6]['id']
+            ]
+        ];
+
+        return $levelsWithAscensions;
+    }
+}
