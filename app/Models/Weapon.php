f479306b6d5febfa6a05cfd4bb09441f855fea55
musiermoore <musiermoore@gmail.com>
2022-04-30 12:53:30 +0600

    Added relations to the weapon model

--- app/Models/Weapon.php
+++ app/Models/Weapon.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class Weapon extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'weapon_type_id',
+        'star_id',
+        'name',
+        'slug',
+        'main_characteristic_id',
+        'description'
+    ];
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+
+    public function star()
+    {
+        return $this->belongsTo(Star::class);
+    }
+
+    public function mainCharacteristic()
+    {
+        return $this->belongsTo(Characteristic::class);
+    }
+
+    public function weaponType()
+    {
+        return $this->belongsTo(WeaponType::class);
+    }
 }
