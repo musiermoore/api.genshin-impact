f575361df6063535000bf955b2199c31bfb75e9e
musiermoore <markowillz@gmail.com>
2022-07-07 20:23:09 +0600

    Added relations and fillable to the artifact set model

--- app/Models/ArtifactSet.php
+++ app/Models/ArtifactSet.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ArtifactSet extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
