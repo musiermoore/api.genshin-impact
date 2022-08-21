ee93125b701d931fd26f3bc4663d289bc656b200
musiermoore <markowillz@gmail.com>
2022-08-21 19:55:14 +0600

    Created a relation with main characteristics

--- app/Models/ArtifactType.php
+++ app/Models/ArtifactType.php
@@ -14,6 +14,26 @@ class ArtifactType extends Model
         'slug'
     ];
 
+    public const FLOWER_OF_LIFE = 'flower-of-life';
+    public const PLUME_OF_DEATH = 'plume-of-death';
+    public const SANDS_OF_EON = 'sands-of-eon';
+    public const GOBLET_OF_EONOTHEM = 'goblet-of-eonothem';
+    public const CIRCLET_OF_LOGOS = 'circlet-of-logos';
+
+    /**
+     * Each artifact type has one or some characteristics.
+     *
+     * For example:
+     * - flower of life - HP.
+     * - plume of death - ATK
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function mainCharacteristics(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Characteristic::class);
+    }
+
     public static function getArtifactTypeIdBySlug($slug)
     {
         return self::query()
